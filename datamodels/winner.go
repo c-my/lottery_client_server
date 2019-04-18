d26acc04acaa614796cd0e080cce942834cba13b
c-my <cmy1113@outlook.com>
2019-04-18 16:23:59 +0800

    add winner repository

--- datamodels/winner.go
+++ datamodels/winner.go
@@ -1,6 +1,6 @@
 package datamodels
 
 type Winner struct {
-	UID string `json:"uid"`
-	AID string `json:"aid"`
+	UID string `json:"uid" gorm:"primary_key"`
+	AID string `json:"aid" gorm:"primary_key"`
 }
